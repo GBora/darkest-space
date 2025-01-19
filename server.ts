fa5fb390602550b7b366ca2f3b7cf2b74e2eedd5
GBora <gpbora@gmail.com>
2025-01-19 15:48:19 +0200

    new end point

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { IShip } from "./interfaces/IShip.interface";
 import { generateShip } from "./factories/generateShip";
+import { IOfficer } from "./interfaces/IOfficer.interface";
+import { generateOfficer } from "./factories/generateOfficer";
 
 const app = express();
 
@@ -11,6 +13,11 @@ app.get("/", (req, res) => {
 app.get("/ship", (req, res) => {
     let ship: IShip = generateShip(3, 2, 2, 1, 1, 0);
     res.send(ship);
+});
+
+app.get("/officer", (req, res) => {
+  let officer: IOfficer = generateOfficer({});
+  res.send(officer);
 });
 
 app.listen(8000);
